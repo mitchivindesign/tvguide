3411cc7db534d2e9f4ba6f8d5d6eaacacb7676f7
Mitch Ivin <mitchellivin@gmail.com>
2026-03-17 09:43:28 +1000

    v0.0.5

--- js/ui.js
+++ js/ui.js
@@ -203,7 +203,6 @@ function setupScrollLocking(container) {
     let touchStartX = 0;
     let touchStartY = 0;
     let scrollLocked = false;
-    let scrollDirection = null;
     
     const THRESHOLD = 10; // pixels to determine scroll direction
     
@@ -211,7 +210,6 @@ function setupScrollLocking(container) {
         touchStartX = e.touches[0].clientX;
         touchStartY = e.touches[0].clientY;
         scrollLocked = false;
-        scrollDirection = null;
     }, { passive: true });
     
     container.addEventListener('touchmove', (e) => {
@@ -227,23 +225,19 @@ function setupScrollLocking(container) {
             scrollLocked = true;
             
             if (deltaY > deltaX) {
-                // Vertical scroll - disable horizontal
-                scrollDirection = 'vertical';
-                container.style.overflowX = 'hidden';
+                // Vertical scroll detected - lock to vertical only
+                container.style.touchAction = 'pan-y';
             } else {
-                // Horizontal scroll - disable vertical
-                scrollDirection = 'horizontal';
-                container.style.overflowY = 'hidden';
+                // Horizontal scroll detected - lock to horizontal only
+                container.style.touchAction = 'pan-x';
             }
         }
     }, { passive: true });
     
     container.addEventListener('touchend', () => {
         // Re-enable both scroll directions
-        container.style.overflowX = 'auto';
-        container.style.overflowY = 'auto';
+        container.style.touchAction = 'pan-x pan-y';
         scrollLocked = false;
-        scrollDirection = null;
     }, { passive: true });
 }
 
